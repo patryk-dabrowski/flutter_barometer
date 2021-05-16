89f4741b39f4de0d270624d6171fd88d71f94f2e
Patryk Dabrowski <patrykdabrowski58@gmail.com>
2021-05-16 16:38:12 +0200

    Initial commit

---
+++ ios/Classes/FlutterBarometerPlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterBarometerPlugin.h"
+
+@implementation FlutterBarometerPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_barometer"
+            binaryMessenger:[registrar messenger]];
+  FlutterBarometerPlugin* instance = [[FlutterBarometerPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
